ec403983af6086c345d25de9f2f942d9e5d5ea89
laxmankumawat3 <laxmankumawat4444@gmail.com>
2022-07-14 12:39:00 +0530

    global folder create in nodejs folder and create file and read file operation done

--- createaserver/index.js
+++ createaserver/index.js
@@ -1,11 +1,25 @@
 const fs = require("fs");
-const input = process.argv;
-if (input[2]=='add'){
-  fs.writeFileSync(input[3],input[4])
-}else if(input[2]=="remove"){
-  fs.unlinkSync(input[3])
-}else{
-  console.log("invalied input")
-}
+const path = require("path");
+
+const dirPath = path.join(__dirname,"global")
+
+// console.log(dirPath);
+
+// for (i=0;i<5; i++){
+//   fs.writeFileSync(dirPath + "/hello"+i+".txt", "a simple text file")
+// }
+
+// it return array of file we  have create them
+fs.readdir(dirPath,(err, files)=>{
+  // console.log(files)
+
+  // it print one by one
+  files.forEach((items)=>{
+console.log("the file is ",items)
+  })
+  
+
+})
+
 
 
